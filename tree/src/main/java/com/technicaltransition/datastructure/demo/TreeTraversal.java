dfadb88dc72cf2fa8bad29e3dc0ba90e1e240596
asharma11 <asharma11@atlassian.com>
2022-12-06 22:44:28 +0530

    add project structure

---
+++ tree/src/main/java/com/technicaltransition/datastructure/demo/TreeTraversal.java
@@ -0,0 +1,7 @@
+package com.technicaltransition.datastructure.demo;
+public class TreeTraversal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello... tree tutorial....");
+    }
+}
